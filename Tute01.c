07c86e657e8b5e2b84b3b018ea5144d2d9895011
IT22098764 <127776710+IT22098764@users.noreply.github.com>
2023-03-14 14:44:25 +0530

    Update Tute01.c

--- Tute01.c
+++ Tute01.c
@@ -5,7 +5,15 @@
 #include <stdio.h>
 
 int main() {
-  
+   int mark1,mark2,SUM;
+   float AVG;
+   printf("Enter first mark");
+   scanf("%d",&mark1);
+   printf("Enter second mark");
+   scanf("%d",&mark2);
+   SUM=mark1+mark2;
+   AVG=(float)SUM/2.0;
+   printf("the average of marks is %.2f",AVG);
   return 0;
 }
 
